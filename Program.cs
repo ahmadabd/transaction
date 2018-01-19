e1653cd3aff00c81c8f76a92d1b121ccb802c6e0
ahmad abd <ahmadabd@users.noreply.github.com>
2018-01-19 15:53:47 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace mahi
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=bank;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            //SqlTransaction transaction;
+            //transaction = con.BeginTransaction("SampleTransaction");
+
+            Console.Write("Enter id:");
+            int id = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter val:");
+            int val = Int32.Parse(Console.ReadLine());
+
+            cmd.CommandText = "begin transaction update account with (rowlock) set account.balance = account.balance + '" + val+"' where account.account_number = '"+id+"'";
+            cmd.ExecuteNonQuery();
+
+            Console.Write("Enter 1 for commit:");
+            int commit = Int32.Parse(Console.ReadLine());
+
+            if(commit == 1)
+            {
+                cmd.CommandText = "commit";
+                cmd.ExecuteNonQuery();
+            }
+
+            con.Close();
+        }
+    }
+}
